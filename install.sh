c64fd0cf11df8ad49e95239bb5ce3f509ba3853a
Blackswan99 <37673853+Blackswan99@users.noreply.github.com>
2018-05-02 21:29:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cp RAVEscan /usr/bin
 
 # Creating a rule to run everytime a USB device is plugged in
 if ! [ -f /etc/udev/rules.d/80-RAVE-scan.rules]; then
-  echo 'ACTION=="add", KERNELS=="sd?", KERNEL=="sd?1", SUBSYSTEM=="block",  RUN ="/usr/bin/RAVEscan"'
+  echo 'ACTION=="add", KERNELS=="sd?", KERNEL=="sd?1", SUBSYSTEM=="block",  RUN ="/usr/bin/RAVEscan"' | tee /etc/udev/rules.d/80-RAVE-scan.rules
   if [ $? -e 0 ]; then
     chmod a+x /usr/bin/RAVEscan
   else
